4ce111ecf55b1244222c9e529253138dc1d7b1a1
droid-dhruv <dhruv.popuri@drdroid.io>
2025-08-11 12:44:25 +0530

    fix in proto codegen

--- drdroid_debug_toolkit/core/proto_codegen.sh
+++ drdroid_debug_toolkit/core/proto_codegen.sh
@@ -8,17 +8,17 @@ PROTO_REPO_DIR="$repo_root/drdroid_debug_toolkit/core/protos"
 venv_dir="/tmp/proto_codegen_venv"
 
 
-cd "$repo_root"/
+cd "$repo_root/drdroid_debug_toolkit/core/"
 
 # clean up old generated code
-find "$repo_root"/drdroid_debug_toolkit/core/protos/ -regex ".*_pb2.*\.pyi?" -exec rm {} +
+find "$repo_root/drdroid_debug_toolkit/core/protos/" -regex ".*_pb2.*\.pyi?" -exec rm {} +
 
 # generate proto code for all protos
 all_protos=$(find "$PROTO_REPO_DIR" -iname "*.proto")
 python -m grpc_tools.protoc \
     -I "$repo_root/drdroid_debug_toolkit/core/" \
-    --python_out="$repo_root/drdroid_debug_toolkit/core/" \
-    --mypy_out="$repo_root/drdroid_debug_toolkit/core/" \
+    --python_out=. \
+    --mypy_out=. \
     $all_protos
 
 echo "Latest proto generation done."
