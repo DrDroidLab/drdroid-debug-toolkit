2b5499dd861a818e25de715a1ef1abf602fbf4c5
droid-dhruv <dhruv.popuri@drdroid.io>
2025-11-20 14:13:43 +0530

    testing

--- drdroid_debug_toolkit/core/integrations/source_managers/cloudwatch_source_manager.py
+++ drdroid_debug_toolkit/core/integrations/source_managers/cloudwatch_source_manager.py
@@ -266,7 +266,6 @@ class CloudwatchSourceManager(SourceManager):
                               is_optional=True),
                 ],
                 'permission_checks': {
-                    'test_cloudwatch_list_dashboards_permission': [{'client_type': 'cloudwatch'}],
                     'test_cloudwatch_list_metrics_permission': [{'client_type': 'cloudwatch'}]
                 }
             },
