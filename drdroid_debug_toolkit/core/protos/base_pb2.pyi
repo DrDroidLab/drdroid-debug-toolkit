cbc18cc7edce685a2c7e1dbafded6224a2b82fc7
Prateek Kanurkar <prateek.kanurkar@drdroid.io>
2026-02-24 15:54:59 +0530

    Add Grafana Tempo datasource integration with 7 task types

--- drdroid_debug_toolkit/core/protos/base_pb2.pyi
+++ drdroid_debug_toolkit/core/protos/base_pb2.pyi
@@ -667,6 +667,7 @@ class _SourceModelTypeEnumTypeWrapper(google.protobuf.internal.enum_type_wrapper
     GRAFANA_PROMETHEUS_DATASOURCE: _SourceModelType.ValueType  # 304
     GRAFANA_ALERT_RULE: _SourceModelType.ValueType  # 305
     GRAFANA_LOKI_DATASOURCE: _SourceModelType.ValueType  # 306
+    GRAFANA_TEMPO_DATASOURCE: _SourceModelType.ValueType  # 307
     CLICKHOUSE_DATABASE: _SourceModelType.ValueType  # 401
     """Clickhouse Models"""
     CLICKHOUSE_TABLE: _SourceModelType.ValueType  # 402
@@ -888,6 +889,7 @@ GRAFANA_TARGET_METRIC_PROMQL: SourceModelType.ValueType  # 303
 GRAFANA_PROMETHEUS_DATASOURCE: SourceModelType.ValueType  # 304
 GRAFANA_ALERT_RULE: SourceModelType.ValueType  # 305
 GRAFANA_LOKI_DATASOURCE: SourceModelType.ValueType  # 306
+GRAFANA_TEMPO_DATASOURCE: SourceModelType.ValueType  # 307
 CLICKHOUSE_DATABASE: SourceModelType.ValueType  # 401
 """Clickhouse Models"""
 CLICKHOUSE_TABLE: SourceModelType.ValueType  # 402
